79a83753e5318fca723c77918ca5139f56385563
yhernandezlez <yhernandezlez@gmail.com>
2015-07-21 23:16:47 +0100

    Despliegue BD en Heroku

---
+++ models/quiz.js
@@ -0,0 +1,8 @@
+// Definición del modelo de quiz
+
+module.exports = function(sequelize,DataTypes) {
+	return sequelize.define('Quiz',
+		{ 	pregunta: 	DataTypes.STRING,
+			respuesta: 	DataTypes.STRING,
+		});
+}
