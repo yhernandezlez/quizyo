c931c2de3b542e6d3a1ca410846e9c1b7d43d9bf
yhernandezlez <yhernandezlez@gmail.com>
2015-07-29 22:35:52 +0100

    Autoload con find

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,7 +2,7 @@ var models = require('../models/models.js');
 
 //Autoload - factoriza el código si ruta incluye :quizId
 exports.load = function(req, res, next, quizId){
-	models.Quiz.findById(quizId).then(
+	models.Quiz.find(quizId).then(
 		function(quiz) {
 			if (quiz) {
 				req.quiz = quiz;
