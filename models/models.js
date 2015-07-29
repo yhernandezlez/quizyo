935bd238d782e03a48254c447996531072760a0b
yhernandezlez <yhernandezlez@gmail.com>
2015-07-29 19:10:38 +0100

    Mod7 Lista de preguntas

--- models/models.js
+++ models/models.js
@@ -40,6 +40,12 @@ sequelize.sync().then(function() {
 			Quiz.create({ pregunta: 'Capital de Italia',
 						  respuesta: 'Roma'	
 						})
+			Quiz.create({ pregunta: 'Capital de Peru',
+						  respuesta: 'Lima'	
+						})
+			Quiz.create({ pregunta: 'Capital de Francia',
+						  respuesta: 'Paris'	
+						})
 			.then(function(){console.log('Base de datos inicializada.')});
 		};
 	});
