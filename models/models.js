be8e290cfda00b673cf101721d25c68508844fc3
yhernandezlez <yhernandezlez@gmail.com>
2015-07-29 19:22:46 +0100

    Mod7 Lista de preguntas quite una pregunta

--- models/models.js
+++ models/models.js
@@ -40,9 +40,6 @@ sequelize.sync().then(function() {
 			Quiz.create({ pregunta: 'Capital de Italia',
 						  respuesta: 'Roma'	
 						})
-			Quiz.create({ pregunta: 'Capital de Peru',
-						  respuesta: 'Lima'	
-						})
 			Quiz.create({ pregunta: 'Capital de Francia',
 						  respuesta: 'Paris'	
 						})
