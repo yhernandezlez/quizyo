a15644e84d0dec4425206c77ae83b68cdab2971a
yhernandezlez <yhernandezlez@gmail.com>
2015-07-29 19:42:00 +0100

    Mod7 Lista de preguntas añado pregunta tras reiniciar bd

--- models/models.js
+++ models/models.js
@@ -40,6 +40,9 @@ sequelize.sync().then(function() {
 			Quiz.create({ pregunta: 'Capital de Italia',
 						  respuesta: 'Roma'	
 						})
+			Quiz.create({ pregunta: 'Capital de Peru',
+						  respuesta: 'Lima'	
+						})
 			Quiz.create({ pregunta: 'Capital de Francia',
 						  respuesta: 'Paris'	
 						})
