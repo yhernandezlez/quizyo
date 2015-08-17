bf96edf6058418e790f856131242233f01003f36
yhernandezlez <yhernandezlez@gmail.com>
2015-08-17 01:02:37 +0100

    mod 9.añadir comentarios

---
+++ models/comment.js
@@ -0,0 +1,11 @@
+//Definicion del modelo de Comment con validacion
+module.exports = function(sequelize,DataTypes) {
+	return sequelize.define(
+		'Comment',
+		{ 	texto: 	{
+				type: DataTypes.STRING,
+				validate: { notEmpty: {msg: "¡Falta el texto del comentario!"}}
+			}
+		}
+	);
+}
